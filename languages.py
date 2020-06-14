2361f673d3dfaa5aa6275c8eacaa193d278036c9
Bion Howard <bion@bitpharma.com>
2020-06-14 19:28:52 -0400

    initial commit

---
+++ languages.py
@@ -0,0 +1,16 @@
+import json, os
+
+FILEPATH = os.path.join(os.path.dirname(__file__), "languages.json")
+
+def load_languages(filepath=FILEPATH):
+    with open(filepath) as languagefile:
+        languages = json.load(languagefile)
+    for l in languages.keys():
+        words = languages[l]['words']
+        words_with_spaces = map(lambda w: w.replace("_", " "), words)
+        languages[l]['words'] = list(words_with_spaces)
+    return languages
+
+if __name__ == "__main__":
+    languages = load_languages()
+    print(languages)
